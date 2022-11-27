d3f694aaca63c1d0ceec1c077223f034aca82964
zhangminke <zhangminkefromflydish@gmail.com>
2022-11-27 21:53:24 +0800

    fix some problem to make sonarcloud work.

--- src/main/java/org/apache/flink/connector/redis/util/DoubleBuffer.java
+++ src/main/java/org/apache/flink/connector/redis/util/DoubleBuffer.java
@@ -31,7 +31,6 @@ public class DoubleBuffer<E> {
 				try {
 					wait(2000);
 				} catch (InterruptedException e) {
-					e.printStackTrace();
 					Thread.currentThread().interrupt();
 				}
 			}
